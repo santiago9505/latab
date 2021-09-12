01a070bbd8d0c8a0b075a5c2ca18d43c8fbf1dae
Santigeek <santigeek@hotmail.com>
2021-09-12 15:20:11 -0500

    adding header section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//styles
+import "../../assets/styles/Header.css"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Hola</h1>
+    </header>
+  )
+}
+
+export default Header
